505c60a7731386be25db929e9a9a0c076a1af7ee
Samir Badi <Samirbadi1@outlook.com>
2022-09-30 15:31:36 -0400

    Fix the overstack of the card in the hand of the player

--- Forbidden Forest/Assets/Scripts/Card/CardHandler.cs
+++ Forbidden Forest/Assets/Scripts/Card/CardHandler.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 public class CardHandler : MonoBehaviour
 {
     [SerializeField] private float cardSpeed = 35f;
-    [SerializeField] private float handWidth = 12f;
+    [SerializeField] private float distanceBetweenCard = 1.25f;
 
     [Header("Transform")]
     [SerializeField] private Transform deckPosition;
@@ -21,12 +21,17 @@ public class CardHandler : MonoBehaviour
     private List<CardObject> cardsObj;
     private List<CardObject> movingToHand;
     private List<CardObject> movingToGraveyard;
+    private List<CardObject> cardInHand;
+    private List<CardObject> cardToMoveInHand;
 
     void Start()
     {
         cardsObj = new List<CardObject>();
         movingToHand = new List<CardObject>();
         movingToGraveyard = new List<CardObject>();
+        cardInHand = new List<CardObject>();
+        cardToMoveInHand = new List<CardObject>();
+
         CombatSystem.Instance.OnDrawCard += OnDrawCard;
         CombatSystem.Instance.OnEnemyTurn += OnEnemyTurn;
     }
@@ -35,6 +40,7 @@ public class CardHandler : MonoBehaviour
     {
         MovingCardFromDeckToHand();
         MovingCardFromHandToGraveyard();
+        MovingCardInHand();
     }
 
     private void OnEnemyTurn(object sender, EventArgs e)
@@ -48,6 +54,8 @@ public class CardHandler : MonoBehaviour
         }
 
         movingToHand = new List<CardObject>();
+        cardInHand = new List<CardObject>();
+        cardToMoveInHand = new List<CardObject>();
     }
 
     private void OnDrawCard(object sender, EventArgs e)
@@ -59,15 +67,13 @@ public class CardHandler : MonoBehaviour
 
     private void MovingCardFromDeckToHand()
     {
-        for(int i = 0; i < movingToHand.Count; i++)
+        for (int i = 0; i < movingToHand.Count; i++)
         {
             CardObject card = movingToHand[i];
-            float step = cardSpeed * Time.deltaTime;
-            card.transform.position = Vector3.MoveTowards(card.transform.position, handParent.position, step);
-
-            if (Vector3.Distance(card.transform.position, handParent.position) < 0.001f)
+            if (MoveCardToTarget(card, handParent.transform.position))
             {
                 movingToHand.RemoveAt(i);
+                AddCardToHand(card);
                 card.transform.SetParent(handParent);
                 i--;
             }
@@ -76,18 +82,53 @@ public class CardHandler : MonoBehaviour
 
     private void MovingCardFromHandToGraveyard()
     {
-        for(int i = 0; i < movingToGraveyard.Count; i++)
+        for (int i = 0; i < movingToGraveyard.Count; i++)
         {
             CardObject card = movingToGraveyard[i];
-            float step = cardSpeed * Time.deltaTime;
-            card.transform.position = Vector3.MoveTowards(card.transform.position, graveyardParent.position, step);
-
-            if (Vector3.Distance(card.transform.position, graveyardParent.position) < 0.001f)
+            if (MoveCardToTarget(card, graveyardParent.transform.position))
             {
                 movingToGraveyard.RemoveAt(i);
                 Destroy(card.gameObject);
                 i--;
             }
         }
+    }
+
+    private void MovingCardInHand(){
+        if(cardToMoveInHand.Count == 0)
+            return;
+
+        Vector3 move = Vector3.left * -distanceBetweenCard; 
+        Vector3 initPos= new Vector3((int)(cardInHand.Count / 2) * distanceBetweenCard, 0f);
+        if(cardInHand.Count % 2 == 0) initPos.x -= distanceBetweenCard / 2; 
+        initPos += handParent.transform.position; 
+
+        for(int i = 0; i < cardToMoveInHand.Count; i++){
+            CardObject card = cardToMoveInHand[i];
+            int index = cardInHand.IndexOf(card);
+            if(MoveCardToTarget(card, initPos - move * index)){
+                cardToMoveInHand.Remove(card);
+                i--;
+            }
+        }
+    }
+
+    private void AddCardToHand(CardObject card)
+    {
+        cardInHand.Add(card);
+        cardToMoveInHand = new List<CardObject>(cardInHand);
+    }
+
+    private bool MoveCardToTarget(CardObject card, Vector3 target)
+    {
+        float step = cardSpeed * Time.deltaTime;
+        card.transform.position = Vector3.MoveTowards(card.transform.position, target, step);
+
+        if (Vector3.Distance(card.transform.position, target) < 0.001f)
+        {
+            card.transform.position = target;
+            return true;
+        } 
+        return false;
     }
 }
